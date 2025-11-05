96b44ed2953cdace723b23ecf902c9971718e0ed
202301473 <202301473@dau.ac.in>
2025-11-05 18:43:23 +0530

    fixed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/', // Your backend URL
-    timeout: 10000, // Optional: timeout after 10 seconds
+    baseURL: import.meta.env.VITE_API_BASE_URL ||   'http://localhost:8000/api/', // Your backend URL
+        timeout: 10000, // Optional: timeout after 10 seconds
     headers: {
         'Content-Type': 'application/json',
     }
